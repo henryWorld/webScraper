29af5b85566f8ebd6a099d5b87ca9d60faba2727
henryje <kanohen@gmail.com>
2019-03-18 13:45:51 +0000

    Code refactoring:removing unnecessary methods and fields

--- src/main/java/sainsbury/scrapper/service/SpringBootConsoleApplication.java
+++ src/main/java/sainsbury/scrapper/service/SpringBootConsoleApplication.java
@@ -1,9 +1,9 @@
 package sainsbury.scrapper.service;
 
+import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -14,18 +14,13 @@ import sainsbury.scrapper.api.Scraper;
  */
 
 @SpringBootApplication
+@RequiredArgsConstructor
 @ComponentScan(basePackages = {"sainsbury.scrapper.api"})
 public class SpringBootConsoleApplication implements CommandLineRunner {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(SpringBootConsoleApplication.class);
     private static String SAINSBURY_URL = "https://jsainsburyplc.github.io/serverside-test/site/www.sainsburys.co.uk/webapp/wcs/stores/servlet/gb/groceries/berries-cherries-currants6039.html";
-
-    private Scraper scraper;
-
-    @Autowired
-    public SpringBootConsoleApplication(Scraper scraper) {
-        this.scraper = scraper;
-    }
+    private final Scraper scraper;
 
     @Override
     public void run(String... args) {
@@ -35,7 +30,6 @@ public class SpringBootConsoleApplication implements CommandLineRunner {
                 SAINSBURY_URL = args[0];
             }
             scraper.scrape(SAINSBURY_URL);
-
         } catch (Exception e) {
             LOGGER.error("An error has occured", e);
         }
